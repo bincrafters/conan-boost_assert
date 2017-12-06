d51c4a55b4805b2fb6c7022a4317d249536b2526
Rene Rivera <grafikrobot@gmail.com>
2017-12-06 08:13:44 -0600

    Require generator.

--- conanfile.py
+++ conanfile.py
@@ -6,6 +6,7 @@ class BoostAssertConan(ConanFile):
     version = "1.65.1"
 
     requires = \
+        "Boost.Generator/1.65.1@bincrafters/testing", \
         "Boost.Config/1.65.1@bincrafters/testing"
 
     lib_short_names = ["assert"]
@@ -14,23 +15,22 @@ class BoostAssertConan(ConanFile):
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-assert"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
     license = "www.boost.org/users/license.html"
     short_paths = True
     build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
-
-    def package_id(self):
-        if self.is_header_only:
-            self.info.header_only()
 
     @property
     def env(self):
         try:
             with tools.pythonpath(super(self.__class__, self)):
-                import boostgenerator # pylint: disable=F0401
+                import boostgenerator  # pylint: disable=F0401
                 boostgenerator.BoostConanFile(self)
         except:
             pass
         return super(self.__class__, self).env
+
+    def package_id(self):
+        self.info.header_only()
 
     # END
